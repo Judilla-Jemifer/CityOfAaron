e51321fc24ccd7b2f691f47e822f9267ea91e81a
Jacalyn <Jacalyn@lady-ozmas-mbp.home>
2018-02-03 18:39:42 -0500

    Fixed Game. It got wacky. 2/3 6:40 PM

--- src/byu/cit260/cityOfAaron/model/Game.java
+++ src/byu/cit260/cityOfAaron/model/Game.java
@@ -1,82 +1,9 @@
-
 package byu.cit260.cityOfAaron.model;
 import byu.cit260.cityOfAaron.model.*;
  /*
   *City of Aaron*
   */
 
-<<<<<<< HEAD
- import java.io.Serializable;
- import java.util.ArrayList;
-
- public class Game implements Serializable{
- //attributes
-   private Map theMap;
-   private Player thePlayer;
-   private CropData cropData;
-   private TeamMember team[];
-     //The Game class does not really need to include a reference to the TeamMember enum.
-     // You can safely ignore this in the class diagram. - Roger deBry
-   private ArrayList<ListItem> animals;
-   private ArrayList<ListItem> tools;
-   private ArrayList<ListItem> provisions;
- //constructor
-   public Game(){}
- //getters and setters
-   public Map getTheMap(){
-     return theMap;
-   }
-   public void setTheMap(Map theMap){
-     this.theMap = theMap;
-   }
-
-   public Player getThePlayer(){
-     return thePlayer;
-   }
-   public void setThePlayer(Player thePlayer){
-     this.thePlayer = thePlayer;
-   }
-
-   public CropData getCropData(){
-     return cropData;
-   }
-   public void setCropData(CropData cropData){
-     this.cropData = cropData;
-   }
-   /*get TeamMember here
-
-   set TeamMember here */
-
-   public ArrayList<ListItem> getAnimals(){
-     return animals;
-   }
-   public void setAnimals(ArrayList<ListItem> animals){
-     this.animals = animals;
-   }
-
-   public ArrayList<ListItem> getTools(){
-     return tools;
-   }
-   public void setTools(ArrayList<ListItem> tools){
-     this.tools = tools;
-   }
-   public ArrayList<ListItem> getProvisions(){
-     return provisions;
-   }
-   public void setProvisions(ArrayList<ListItem> provisions){
-     this.provisions = provisions;
-   }
-
-
-   //I removed the main() function here and put it in the main class CityOfAaron.java
-   
-
-
-
-   }
- 
-
-=======
   import java.io.Serializable;
   import java.util.ArrayList;
 
@@ -136,4 +63,3 @@ import byu.cit260.cityOfAaron.model.*;
       this.provisions = provisions;
     }
   }
->>>>>>> b598e3b2a5ccf13c7628d93296199e89ffecf2f1

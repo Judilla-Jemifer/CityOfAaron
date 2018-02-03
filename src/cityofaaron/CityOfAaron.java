198d7c1df36ed420f7b9caf91c4056ec42b6e92a
Judilla-Jemifer <jemjudilla@hotmail.com>
2018-02-03 17:04:16 -0600

    ListItem Instance

--- src/cityofaaron/CityOfAaron.java
+++ src/cityofaaron/CityOfAaron.java
@@ -1,17 +1,17 @@
 
 package cityofaaron;
 
-<<<<<<< HEAD
+
 
 /**
  *
  * @authors Jemifer/Jacalyn/Aaron
  */
-=======
+
 //The * tells it to import all files in the model folder
 //Upload to the dang website, NetBeans! 
 import byu.cit260.cityOfAaron.model.*;
->>>>>>> a5d58fe7dcf5d167e67dd6b13041773bfb01f202
+
 
 public class CityOfAaron {
 
@@ -19,70 +19,6 @@ public class CityOfAaron {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-
-        Player playerOne = new Player();
-    playerOne.setName("Alma The Missionary");
-
-    String playerOneName = playerOne.getName();
-    System.out.println(playerOne.toString());
-
-    //set Location instance - Jacalyn
-    Location location = new Location();
-    location.setDescription("The City of Aaron, near Nephihah");
-
-    String locationDescription = location.getDescription();
-    System.out.println(location.toString());
-
-
-    //From Jem
-    ListItem[] animalsOne = New ListItem[ItemCount];
-
-    //for animals
-
-<<<<<<< HEAD
-    animalsOne.setName("sheep");
-    animalsOne.setNumber(12.00);
-=======
-      //From Jem
-      ListItem animalsOne = new ListItem();
->>>>>>> a5d58fe7dcf5d167e67dd6b13041773bfb01f202
-
-    String animalsOneName = animalsOne.getName();
-    double animalsOneNumber = animalsOne.getNumber();
-
-    System.out.println(animalsOne.toString());
-
-    //for tools
-    ListItem[] toolsOne = New ListItem[ItemCount];
-
-    toolsOne.setName("sword");
-    toolsOne.setNumber(7.00);
-
-    String animalsOneName = toolsOne.getName();
-    double animalsOneNumber = toolsOne.getNumber();
-
-    System.out.println(toolsOne.toString());
-
-    //for provisions
-    ListItem[] provisionsOne = New ListItem[ItemCount];
-
-    provisionsOne.setName("helmet");
-    provisionsOne.setNumber(7.00);
-
-    String provisionsOneName = provisionsOne.getName();
-    double provisionsOneNumber = provisionsOne.getNumber();
-
-    System.out.println(toolsOne.toString());
-    }
-
-}
-
-//set ListItem instance 
-//I hope I'm doing this right because I'm not sure how to do it for Arrays
-
-
-
-public static void main(String[] args) {
 
     Player playerOne = new Player();
     playerOne.setName("Alma The Missionary");
@@ -98,20 +34,40 @@ public static void main(String[] args) {
     System.out.println(location.toString());
 
 
+
     //From Jem
-    ListItem[] animalsOne = New ListItem[ItemCount];
-
-    //From Jem - tried to change it to make just one instance
-    ListItem[] listItemOne = New ListItem[];
-
-    listItemOne.setName("sheep");
-    listItemOne.setNumber(12.00);
-
-    String listItemName = listItemOne.getName();
-    double listItemOneNumber = listItemOne.getNumber();
-
-    System.out.println(listItemOne.toString());
+    ListItem animalsOne = new ListItem();
+      
+    animalsOne.setName("sheep");
+    animalsOne.setNumber(12.00);
 
 
+    String animalsOneName = animalsOne.getName();
+    double animalsOneNumber = animalsOne.getNumber();
+
+    System.out.println(animalsOne.toString());
+
+    //for tools
+    ListItem toolsOne = new ListItem();
+
+    toolsOne.setName("sword");
+    toolsOne.setNumber(7);
+
+    String animalsOneName = toolsOne.getName();
+    double animalsOneNumber = toolsOne.getNumber();
+
+    System.out.println(toolsOne.toString());
+
+    //for provisions
+    ListItem provisionsOne = new ListItem();
+
+    provisionsOne.setName("helmet");
+    provisionsOne.setNumber(7);
+
+    String provisionsOneName = provisionsOne.getName();
+    double provisionsOneNumber = provisionsOne.getNumber();
+
+    System.out.println(toolsOne.toString());
+    
   }
 }

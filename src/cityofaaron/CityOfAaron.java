15bdd7cb36467976ccbdf4f4f80108b395b7972f
Judilla-Jemifer <jemjudilla@hotmail.com>
2018-02-02 21:55:14 -0600

    New Package Commit

--- src/cityofaaron/CityOfAaron.java
+++ src/cityofaaron/CityOfAaron.java
@@ -7,7 +7,7 @@ package cityofaaron;
 
 /**
  *
- * @author HP14-ab111tx
+ * @authors Jemifer/Jacalyn/Aaron
  */
 public class CityOfAaron {
 
@@ -15,7 +15,54 @@ public class CityOfAaron {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Player playerOne = new Player();
+    playerOne.setName("Alma The Missionary");
+
+    String playerOneName = playerOne.getName();
+    System.out.println(playerOne.toString());
+
+    //set Location instance - Jacalyn
+    Location location = new Location();
+    location.setDescription("The City of Aaron, near Nephihah");
+
+    String locationDescription = location.getDescription();
+    System.out.println(location.toString());
+
+
+    //From Jem
+    ListItem[] animalsOne = New ListItem[ItemCount];
+
+    //for animals
+
+    animalsOne.setName("sheep");
+    animalsOne.setNumber(12.00);
+
+    String animalsOneName = animalsOne.getName();
+    double animalsOneNumber = animalsOne.getNumber();
+
+    System.out.println(animalsOne.toString());
+
+    //for tools
+    ListItem[] toolsOne = New ListItem[ItemCount];
+
+    toolsOne.setName("sword");
+    toolsOne.setNumber(7.00);
+
+    String animalsOneName = toolsOne.getName();
+    double animalsOneNumber = toolsOne.getNumber();
+
+    System.out.println(toolsOne.toString());
+
+    //for provisions
+    ListItem[] provisionsOne = New ListItem[ItemCount];
+
+    provisionsOne.setName("helmet");
+    provisionsOne.setNumber(7.00);
+
+    String provisionsOneName = provisionsOne.getName();
+    double provisionsOneNumber = provisionsOne.getNumber();
+
+    System.out.println(toolsOne.toString());
     }
     
 }

622efbba46f8b52578232a982e8afaffd036050d
ct <ct@claytons-air.t-mobile.com>
2018-02-12 22:08:31 -0700

    updated class per slides 12-13

--- src/byu/cit260/cityOfAaron/control/CropControl.java
+++ src/byu/cit260/cityOfAaron/control/CropControl.java
@@ -10,6 +10,34 @@ package byu.cit260.cityOfAaron.control;
  * @author ct
  */
 public class CropControl {
-    //comment
-    //comment2
+
+// The sellLand method
+// Purpose: To sell land
+// Parameters: the price of land, the number of acres to sell, and a
+  // reference to a CropData object
+// Returns: the acres owned after the sale
+// Pre-conditions: acres to sell must be positive
+  // and <= acresOwned
+    public static int sellLand(int landPrice, int acresToSell, CropData cropData){
+
+      //if acresToSell < 0, return -1
+      if(acresToSell < 0){
+        return -1;
+      }
+      //if acresToSell > acresOwned, return -1
+      int owned = cropData.getAcresOwned();
+      if(acresToSell > owned){
+        return -1;
+      }
+      //acresOwned = acresOwned - acresToSell
+      owned -= acresToSell;
+      cropData.setAcresOwned(owned);
+      //wheatInStore = wheatInStore + acresToSell * landPrice
+      int wheat = cropData.getWheatInStore();
+      wheat += (acresToSell * landPrice);
+      cropData.setWheatInStore(wheat);
+      //return acresOwned
+      return owned;
+
+    }
 }

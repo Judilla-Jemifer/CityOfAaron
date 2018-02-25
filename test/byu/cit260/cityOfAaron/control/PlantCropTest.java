352a2e28e68513d9007d110c5e5597bd65a39108
Jacalyn <Jacalyn@lady-ozmas-mbp.home>
2018-02-24 20:33:53 -0500

    Jacalyn Boggs 2/24 - Test file prepped to send to professor.

--- test/byu/cit260/cityOfAaron/control/PlantCropTest.java
+++ test/byu/cit260/cityOfAaron/control/PlantCropTest.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package byu.cit260.cityOfAaron.control;
 
 import byu.cit260.cityOfAaron.model.CropData;

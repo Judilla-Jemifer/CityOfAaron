113abce8a39ed2306072566d74d3b9373a3f9c1a
Judilla-Jemifer <jemjudilla@hotmail.com>
2018-03-07 11:58:19 -0600

    displaymenu

--- src/byu/cit260/cityOfAaron/view/MenuView.java
+++ src/byu/cit260/cityOfAaron/view/MenuView.java
@@ -1,18 +1,34 @@
 //abstract super class for all menu views
-package view;
+package byu.cit260.cityOfAaron.view;
 
 import java.util.Scanner;
 
-public abstract class MainMenu implements ViewInterface{
+public abstract class MenuView implements ViewInterface{
+
   //declare all variables common to all menu views
   protected final static Scanner keyboard = new Scanner(System.in);
-  protected String menu;
+  protected String menu; //this string holds the menu string
   protected int max;
 
-  //constructor
+  //MenuView constructor
+  //Purpose: Initialize the view object with the menu string
+  //Parameters: the menu string and the max value
+  //Returns: none
   public MenuView(String _menu, int _max){
       menu = _menu;
       max = _max;
   }
+@Override public void displayMenu(){
+    int menuOption = 0;
+    do{
+      //display the menu
+      System.out.println(menu);
+      //get user selection
+      menuOption = getMenuOption();
+      //perform action
+      doAction(menuOption);
+      //determine and display next view
+      } while (menuOption != max);
 
+  }
 }
